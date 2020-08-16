fdd423c38c59cad0fb3df6acf4ecb6a72acd9341
myshuker <myshuker@gmai.com>
2020-08-17 01:43:11 +0200

    Updates

---
+++ precache-manifest.da7fc936a4556c323bd27f9a737244aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20a8071881b5262f8ee61cee91c6f65",
+    "url": "/ToDoList2/index.html"
+  },
+  {
+    "revision": "fca461f5bc86b3d30f21",
+    "url": "/ToDoList2/static/css/main.c86a73f3.chunk.css"
+  },
+  {
+    "revision": "6374b44df704401b3581",
+    "url": "/ToDoList2/static/js/2.b867d86b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList2/static/js/2.b867d86b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca461f5bc86b3d30f21",
+    "url": "/ToDoList2/static/js/main.3fad32e2.chunk.js"
+  },
+  {
+    "revision": "9ba7096722eb77527844",
+    "url": "/ToDoList2/static/js/runtime-main.ef698d54.js"
+  }
+]);
